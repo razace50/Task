7e5c8b9d79e81e612d39315677c8aaa9326a57bf
Rajesh Sunar <134181496+razace50@users.noreply.github.com>
2025-03-10 12:17:13 +0545

    initial
    initial

---
+++ src/components/dashboard/components/NextButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const NextButton = () => {
+  return (
+    <div>
+      <Button variant={"outline"} type="submit">
+        Next page
+      </Button>
+    </div>
+  );
+};
+
+export default NextButton;
---
+++ src/components/dashboard/components/PrevButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const PrevButton = () => {
+  return (
+    <div>
+      <Button variant={"outline"} type="submit">
+        PreviousPage
+      </Button>
+    </div>
+  );
+};
+
+export default PrevButton;
